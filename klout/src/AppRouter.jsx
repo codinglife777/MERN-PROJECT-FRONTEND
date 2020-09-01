6a941bbc7b58634d8b68df3fd210576d689eaf62
carlo615 <carlo.reynaga06@gmail.com>
2020-09-01 19:37:01 +0200

    Components-files

--- klout/src/AppRouter.jsx
+++ klout/src/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomePage from "./components/Homepage";
+import HomePage from "/components/HomePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export const AppRouter = () => {
