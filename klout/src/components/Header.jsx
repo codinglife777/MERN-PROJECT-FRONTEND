9dfb638096e5ad7fb53be1ffc464ea1814d91689
carlo615 <carlo.reynaga06@gmail.com>
2020-09-03 20:47:56 +0200

    Front Styles

--- klout/src/components/Header.jsx
+++ klout/src/components/Header.jsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import NextStep from "./NextStep";
+
 
 export default class Header extends Component {
   static propTypes = {
@@ -10,29 +11,12 @@ export default class Header extends Component {
   render() {
     const { authenticated } = this.props;
     return (
-      <ul className="menu">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        {authenticated ? (
-          <li onClick={this._handleLogoutClick}>Logout</li>
-        ) : (
-          <li onClick={this._handleSignInClick}>Login</li>
-        )}
-      </ul>
+      
+      <NextStep/>
+      
+      
     );
   }
 
-  _handleSignInClick = () => {
-    // Authenticate using via passport api in the backend
-    // Open Twitter login page
-    window.open("http://localhost:4000/auth/twitter", "_self");
-  };
 
-  _handleLogoutClick = () => {
-    // Logout using Twitter passport api
-    // Set authenticated state to false in the HomePage component
-    window.open("http://localhost:4000/auth/logout", "_self");
-    this.props.handleNotAuthenticated();
-  };
 }
---
+++ klout/src/components/Home.jsx
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+export default function FloatingActionButtons() {
+  const classes = useStyles();
+
+  return (
+   
+  );
+}
