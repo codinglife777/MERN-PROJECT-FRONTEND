8fbf46c19406de56a68cfcd4da71f74f66b4276b
carlo615 <carlo.reynaga06@gmail.com>
2020-09-05 14:16:32 +0200

    Update Logo Component

--- klout/src/AppRouter.jsx
+++ klout/src/AppRouter.jsx
@@ -5,6 +5,7 @@ import SignUp from "./components/SignUp";
 import AddNetworks from "./components/AddNetworks";
 import HomeResume from "./components/HomeResume";
 import SocialProfile from "./components/SocialProfile";
+import Profile from "./components/Profile";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export const AppRouter = () => {
@@ -18,6 +19,7 @@ export const AppRouter = () => {
           <Route exact path="/addnetworks" component={AddNetworks} />
           <Route exact path="/homeresume" component={HomeResume} />
           <Route exact path="/socialprofile" component={SocialProfile} />
+          <Route exact path="/profile" component={Profile} />
         </Switch>
       </div>
     </Router>
